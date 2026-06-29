1857a84336e943f0100186ee880bd726ecaf0ed2
SHRAWEG <swgmaharjan@gmail.com>
2026-06-29 15:39:45 +0545

    added sort for contents

--- src/database/migrations/1782377729295-AddSortOrderToContents.ts
+++ src/database/migrations/1782377729295-AddSortOrderToContents.ts
@@ -1,18 +1,27 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddSortOrderToContents1782377729295 implements MigrationInterface {
-    name = 'AddSortOrderToContents1782377729295'
+  name = 'AddSortOrderToContents1782377729295';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "child_contents" ADD "sort_order" integer NOT NULL DEFAULT '0'`);
-        await queryRunner.query(`ALTER TABLE "contents" ADD "sort_order" integer NOT NULL DEFAULT '0'`);
-        await queryRunner.query(`ALTER TABLE "contents" ALTER COLUMN "html_content" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "child_contents" ADD "sort_order" integer NOT NULL DEFAULT '0'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "contents" ADD "sort_order" integer NOT NULL DEFAULT '0'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "contents" ALTER COLUMN "html_content" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "contents" ALTER COLUMN "html_content" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "contents" DROP COLUMN "sort_order"`);
-        await queryRunner.query(`ALTER TABLE "child_contents" DROP COLUMN "sort_order"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "contents" ALTER COLUMN "html_content" SET NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE "contents" DROP COLUMN "sort_order"`);
+    await queryRunner.query(
+      `ALTER TABLE "child_contents" DROP COLUMN "sort_order"`,
+    );
+  }
 }
